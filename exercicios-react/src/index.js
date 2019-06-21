1e635804a7a988d92af550060ab3a136bc7ac934
EzequielGnich <ezequielgnich@live.com>
2019-06-21 12:05:36 -0300

    Exportação de múltiplos elementos

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import PrimeiroComponente from './components/PrimeiroComponent'
+// import PrimeiroComponente from './components/PrimeiroComponent'
+import { CompA, CompB as B } from './components/DoisComponentes'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
   <div>
-    <PrimeiroComponente valor="Bom dia!" valor2={2**4}/>
+    {/*<PrimeiroComponente valor="Bom dia!" valor2={2**4}/> */}
+  
+    <CompA valor="Olá eu sou A!" />
+    <B valor="Olá eu sou B!" />
   </div>
+
 , elemento)
 
 // ReactDOM.render(jsx, elemento)
