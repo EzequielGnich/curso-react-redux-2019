045e506019bae454b72d1b9975fcd74930137517
EzequielGnich <ezequielgnich@live.com>
2019-06-22 18:00:13 -0300

    Relação entre componentes React 2

--- exercicios-react/src/components/Familia.jsx
+++ exercicios-react/src/components/Familia.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
-import Membro from './Membro'
-
 export default props =>
   <div>
-    <Membro nome="Ezequiel" sobrenome="Gnich"/>
-    <Membro nome="João" sobrenome="Silva"/>
-    <Membro nome="Carlos" sobrenome="Silva"/>
-    <Membro nome="Larissa" sobrenome="Silva"/>
+    <h1>Família {props.sobrenome}</h1>
+    {props.children}
   </div>
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -4,15 +4,26 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './components/PrimeiroComponent'
 // import { CompA, CompB as B } from './components/DoisComponentes'
 // import MultiElementos from './components/MultiElementos'
+// import Familia from './components/FamiliaG'
 
 import Familia from './components/Familia'
+import Membro from './components/Membro'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
   <div>
 
-    <Familia />
+    <Familia sobrenome="Gnich">
+      <Membro nome="Ezequiel" sobrenome="Gnich" />
+      <Membro nome="Olivie" sobrenome="Gnich" />
+    </Familia>
+    <Familia sobrenome="Albert">
+      <Membro nome="Gabriela" sobrenome="Albert" />
+      <Membro nome="Cristiano" sobrenome="Albert" />
+    </Familia>
+
+    {/*<Familia />*/}
 
     {/*<MultiElementos />*/}
     
