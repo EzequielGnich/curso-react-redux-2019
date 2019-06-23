54cf6e043f5c7ed6e1ae6144261ae94b31185b1b
EzequielGnich <ezequielgnich@live.com>
2019-06-23 18:59:42 -0300

    Soluções para this

--- exercicios-react/src/components/Counter.jsx
+++ exercicios-react/src/components/Counter.jsx
@@ -1,20 +1,49 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
-  sumOne() {
+
+  sumOne = () => {
     this.props.numero++
+    console.log(this)
   }
-  minusOne() {
+  minusOne = () => {
     this.props.numero--
+    console.log(this)
   }
 
   render() {
     return (
       <div>
-        <div>Número: {this.props.number}</div>
-        <button onClick={this.sumOne}>Inc</button>
-        <button onClick={this.minusOne}>Dec</button>
+        <div>Número: {this.props.numero}</div>
+        <button onClick={() => this.sumOne()}>Inc</button>
+        <button onClick={() => this.minusOne()}>Dec</button>
       </div>
     )
   }
 }
+
+//*****************************************************************//
+//************** Solução utilizando Função Bind *******************//
+// Solução 01
+// constructor(props) {
+//  super(props)
+//  this.sumOne = this.sumOne.bind(this)
+//  this.minusOne = this.minusOne.bind(this)
+//  }
+
+//*****************************************************************//
+//***** Solução utilizando Arrow Function no evento onClick *******//
+//  Solução 2 para this
+//  <button onClick={() => this.sumOne()}>Inc</button>
+//  <button onClick={() => this.minusOne()}>Dec</button>
+
+//*****************************************************************//
+//************ Solução utilizando Arrow Function ******************//
+//  sumOne = () => {
+//  this.props.numero++
+//  console.log(this)
+//  }
+//  minusOne = () => {
+//  this.props.numero--
+//  console.log(this)
+//  }
