77646ed5007d8ec40e336ed7ea4e012ca8a4fd0c
EzequielGnich <ezequielgnich@live.com>
2019-07-06 18:57:17 -0300

    Finalização do backend e adicionando cors na aplicação

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 const bodyParser = require('body-parser')
 
@@ -7,6 +8,7 @@ const port = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
   console.log(`Backend is runing on port ${port}.`)
