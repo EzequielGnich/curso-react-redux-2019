e1e0b56d5cf0d59ba25679cadaa11b8c9e6b62d3
EzequielGnich <ezequielgnich@live.com>
2019-07-14 17:39:13 -0300

    Correção das configurações do webpack, compilando normalmente

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -1,11 +1,10 @@
-import 'modules/bootstrap/dist/css/bootstrap.min.css'
-import 'modules/font-awesome/css/font-awesome.min.css'
-
 import React from 'react'
-import Todo from '../todo/todo'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 export default props => (
   <div className='container'>
-    <Todo />
+    <h1>Hello world!</h1>
   </div>
 )
--- todo-app/frontend/webpack.config.js
+++ todo-app/frontend/webpack.config.js
@@ -1,42 +1,122 @@
+const webpack = require('webpack')
 const path = require('path')
+const TransferWebpackPlugin = require('transfer-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+const autoprefixer = require('autoprefixer')
+const precss = require('precss')
+
 module.exports = {
-  entry: './src/index.jsx',
+  devtool: 'eval',
+  entry: [
+    'webpack/hot/only-dev-server',
+    'tether',
+    'font-awesome/scss/font-awesome.scss',
+    './src/index.jsx'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'app.js'
   },
   devServer: {
+    contentBase: 'public',
+    publicPath: '/',
     inline: true,
-    contentBase: './public',
-    port: 8080
+    port: process.env.PORT || 8080,
+    host: 'localhost',
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('app.css'),
+    new TransferWebpackPlugin([
+      { from: 'public' }
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      tether: 'tether',
+      Tether: 'tether',
+      'window.Tether': 'tether',
+      Popper: ['popper.js', 'default'],
+      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+      Button: 'exports-loader?Button!bootstrap/js/dist/button',
+      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+      Util: 'exports-loader?Util!bootstrap/js/dist/util'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }, {
+        test: /\.(scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader'
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                plugins () {
+                  return [
+                    precss,
+                    autoprefixer
+                  ]
+                }
+              }
+            }, {
+              loader: 'sass-loader'
+            }
+          ]
+        })
+      }, {
+        test: /font-awesome\.config\.js/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'font-awesome-loader' }
+        ]
+      }, {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: 'imports-loader?jQuery=jquery'
+      }, {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000'
+      }, {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader'
+      }, {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug'
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      modules: path.resolve(__dirname, 'node_modules/')
+      modules: path.resolve(__dirname, '/node_modules/')
     }
-  },
-  plugins: [
-    new ExtractTextPlugin('app.css')
-  ],
-  module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: [
-        { loader: 'style-loader' },
-        {
-          query: {
-            presets: ['es2015', 'stage-0']
-          }
-        }
-      ]
-    }, {
-      test: /.(svg|woff|woff2|ttf|eot|otf)([?]?.*)$/,
-      loader: 'file-loader?name=assets/fonts/[name].[ext]'
-    }
-    ]
   }
 }
