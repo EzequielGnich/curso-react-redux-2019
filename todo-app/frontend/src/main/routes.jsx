ec6aefb44738f4a8ff1cc6cf7e16df7537fed948
EzequielGnich <ezequielgnich@live.com>
2019-07-14 22:16:37 -0300

    Atualizado componente routes.jsx, react-route para react-router-dom

--- todo-app/frontend/src/main/routes.jsx
+++ todo-app/frontend/src/main/routes.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {
+  HashRouter,
+  Route,
+  Redirect
+} from 'react-router-dom'
 
 import Todo from '../todo/todo'
 import About from '../about/about'
 
 export default props => (
-  <Router history={Route}>
-    <Route path='/todos' component={Todo} />
-    <Route path='/about' component={About} />
-  </Router>
+  <HashRouter>
+    <div>
+      <Route path='/todos' component={Todo} />
+      <Route path='/about' component={About} />
+      <Redirect from='*' to='/todos' />
+    </div>
+  </HashRouter>
 )
