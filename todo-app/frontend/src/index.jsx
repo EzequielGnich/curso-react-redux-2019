0a3a3a6508dade103e2f6e99f4dfc03c5cd35f83
EzequielGnich <ezequielgnich@live.com>
2019-07-20 17:15:20 -0300

    Finalização do app todo-app

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const reducers = combineReducers({
+  field: () => ({ value: 'Opa' })
+})
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)} >
+    <App />
+  </Provider>
+  , document.getElementById('app'))
