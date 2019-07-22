a183677aa92ffb51612bfa04ee521761f4057197
EzequielGnich <ezequielgnich@live.com>
2019-07-22 00:25:35 -0300

    Criação das actions markAsDone, markAsPending e inserção no arquivo todoList.jsx

--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending } from './todoActions'
 
 const TodoList = props => {
   const renderRows = () => {
@@ -13,12 +16,12 @@ const TodoList = props => {
             style='success'
             icon='check'
             hide={todo.done}
-            onClick={() => props.handleMarkAsDone(todo)} />
+            onClick={() => props.markAsDone(todo)} />
           <IconButton
             style='warning'
             icon='undo'
             hide={!todo.done}
-            onClick={() => props.handleMarkAsPending(todo)} />
+            onClick={() => props.markAsPending(todo)} />
           <IconButton
             style='danger'
             icon='trash-o'
@@ -50,4 +53,7 @@ const mapStateToProps = state => ({
   list: state.todo.list
 })
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ markAsDone, markAsPending }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
