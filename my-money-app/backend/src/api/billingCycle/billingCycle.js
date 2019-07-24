f23d7db9c35612ab83e63ef70deda99fce833f91
EzequielGnich <ezequielgnich@live.com>
2019-07-24 12:19:36 -0300

    Criação da rota /count e /sumary

--- my-money-app/backend/src/api/billingCycle/billingCycle.js
+++ my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -8,7 +8,11 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, 'Informe o valor do débito']
+  },
   status: { type: String,
     required: false,
     uppercase: true,
--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -4,3 +4,13 @@ mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney',
   { useNewUrlParser: true }
 )
+
+// Mensagens de erro traduzidas para PT-BR
+mongoose.Error.messages.general.required =
+"O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min =
+  "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'"
+mongoose.Error.messages.Number.max =
+  "O '{VALUE}' informado é maior que o limite máximo de '{NAX}'"
+mongoose.Error.messages.String.enum =
+  "'{VALUE}' não é válido para o atributo '{PATH}'"
