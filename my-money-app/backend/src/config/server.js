a0a268fbcd11d748b1e3d86db6fb62e2ef4dc67a
EzequielGnich <ezequielgnich@live.com>
2019-07-25 12:31:25 -0300

    incluido express-query-int para evitar qualquer problema na leitura dos parametros passado na requisição

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -2,12 +2,14 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 const port = process.env.PORT || 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function () {
   console.log(`Backend is running on port ${port}`)
