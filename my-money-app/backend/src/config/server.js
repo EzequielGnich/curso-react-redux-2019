e86f20db5c2931864354617d0db7be27a0521181
EzequielGnich <ezequielgnich@live.com>
2019-07-25 12:23:58 -0300

    Implementado Cors

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 const port = process.env.PORT || 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function () {
   console.log(`Backend is running on port ${port}`)
