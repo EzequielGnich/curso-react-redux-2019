5091cec50a632eafedf757cbeb388948fa13a417
EzequielGnich <ezequielgnich@live.com>
2019-07-30 11:20:46 -0300

    Alterado a estrutura para suportar o redux, aplicado middleware para poder carregar as informações vindas do DB

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './main/app'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import promise from 'redux-promise'
 
+import App from './main/app'
+import Reducers from './main/reducers'
+
+const store = applyMiddleware(promise)(createStore)(Reducers)
 const rootElement = document.getElementById('app')
 
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , rootElement)
