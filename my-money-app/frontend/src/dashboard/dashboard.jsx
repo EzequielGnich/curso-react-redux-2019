4b7b749329877785ace827300105b9f7ccd46542
EzequielGnich <ezequielgnich@live.com>
2019-07-30 10:17:12 -0300

    adicionado component valueBox e componente row

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
   render () {
@@ -9,7 +11,14 @@ class Dashboard extends Component {
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0.1' />
         <Content>
-          Dashboard
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10'
+              text='Total de créditos' />
+            <ValueBox cols='12 4' color='red' icon='credit-card' value='-R$ 10'
+              text='Total de débitos' />
+            <ValueBox cols='12 4' color='blue' icon='money' value='R$ 1.250,23'
+              text='Valor consolidado' />
+          </Row>
         </Content>
       </div>
     )
