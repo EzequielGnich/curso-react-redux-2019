4a634726370747858649166dc943934d89360d22
EzequielGnich <ezequielgnich@live.com>
2019-08-12 12:31:21 -0300

    Adicionado componente responsavel por passar o header das abas

---
+++ my-money-app/frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class TabHeader extends Component {
+  render() {
+    return (
+      <li>
+        <a
+          href='javascript:;'
+          data-toggle='tab'
+          data-target={this.props.target}>
+          <i className={`fa fa-${this.props.icon}`} />
+          { this.props.label }
+        </a>
+      </li>
+    )
+  }
+}
