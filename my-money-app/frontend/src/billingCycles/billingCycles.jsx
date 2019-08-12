f281edb7a37782f856ee81ffd4baacbae6d820b7
EzequielGnich <ezequielgnich@live.com>
2019-08-12 12:28:18 -0300

    Instanciado os componentes de Tabs, TabsHeader, TabHeader e TabsContent

--- my-money-app/frontend/src/billingCycles/billingCycles.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycles.jsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <h1>Billing Cycles</h1>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+
+class BillingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Ciclo de pagamentos' small='Cadastro' />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label='Listar' icon='bars' target='tabList' />
+              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+            </TabsHeader>
+            <TabsContent>
+
+            </TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default BillingCycle
