de19047b9039070a297d83d07468fa59d58ddab2
EzequielGnich <ezequielgnich@live.com>
2019-08-16 13:09:34 -0300

    Feito a instanciação dos novos reducers da aplicação (billinCycleReducer, formReducer)

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCyclesReducer from '../billingCycles/billingCyclesReducer'
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
-  tab: TabReducer
+  tab: TabReducer,
+  billingCycle: BillingCyclesReducer,
+  form: formReducer
 })
 
 export default rootReducer
