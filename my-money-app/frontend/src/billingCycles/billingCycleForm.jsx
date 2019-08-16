61215e8a20ba71fb8d706c8b91d7dc986005aebb
EzequielGnich <ezequielgnich@live.com>
2019-08-16 13:01:07 -0300

    Adicionado componente de Form

---
+++ my-money-app/frontend/src/billingCycles/billingCycleForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class billingCycleForm extends Component {
+  render() {
+    return (
+      <form role='form'>
+        <div className="box-body">
+
+        </div>
+        <div className="box-footer">
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default billingCycleForm
