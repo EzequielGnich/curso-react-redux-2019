58dae7d6164bd3b57b4bccb256a7fa2bb3caa0ee
EzequielGnich <ezequielgnich@live.com>
2019-08-16 13:08:38 -0300

    Removido o href da tag <a> pois estava dando conflito com as chamadas das rotas

--- my-money-app/frontend/src/common/template/menuTree.jsx
+++ my-money-app/frontend/src/common/template/menuTree.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => (
   <li className='treeview'>
-    <a href>
+    <a>
       <i className={`fa fa-${props.icon}`} />
       {props.label}
       <i className='fa fa-angle-left pull-right' />
