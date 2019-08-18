6729c586071229b4b7942b0c0b8d598ed0e1adc7
EzequielGnich <ezequielgnich@live.com>
2019-08-18 19:45:44 -0300

    criada a conexão com o react-redux, importado a função init() para resetar os formularios

--- my-money-app/frontend/src/billingCycles/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycleForm.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
 
+import { init } from './billingCyclesActions'
 import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, readOnly } = this.props
     return (
       <form role='form' onSubmit={handleSubmit}>
         <div className="box-body">
@@ -15,29 +18,45 @@ class BillingCycleForm extends Component {
               component={labelAndInput}
               label='Nome'
               cols='12 4'
-              placeholder='Informe o nome' />
+              placeholder='Informe o nome'
+              readOnly={readOnly} />
             <Field
               name='month'
               component={labelAndInput}
               type='number'
               label='Mês'
               cols='12 4'
-              placeholder='Informe o mês' />
+              placeholder='Informe o mês'
+              readOnly={readOnly} />
             <Field
               name='year'
               component={labelAndInput}
               type='number'
               label='Ano'
               cols='12 4'
-              placeholder='Informe o ano'/>
+              placeholder='Informe o ano'
+              readOnly={readOnly} />
           </div>
         </div>
         <div className="box-footer">
-          <button type='submit' className='btn btn-primary'>Submit</button>
+          <button
+            type='submit'
+            className={`btn btn-${this.props.submitClass}`}>
+            {this.props.submitLabel}
+          </button>
+          <button
+            type='button'
+            className='btn btn-default'
+            onClick={this.props.init}>Cancelar</button>
         </div>
       </form>
     )
   }
 }
 
-export default reduxForm({form: 'billingCycleForm'})(BillingCycleForm)
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ init }, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
