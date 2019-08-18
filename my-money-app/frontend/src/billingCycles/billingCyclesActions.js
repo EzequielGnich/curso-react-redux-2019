27cdd6432f868fd88b089e0e26fdc5de6f492f9b
EzequielGnich <ezequielgnich@live.com>
2019-08-18 17:52:27 -0300

    Feita integração com o react-redux-toastr para mostrar mensagens na tela

--- my-money-app/frontend/src/billingCycles/billingCyclesActions.js
+++ my-money-app/frontend/src/billingCycles/billingCyclesActions.js
@@ -1,10 +1,34 @@
 import axios from 'axios'
 const BASE_URL = 'http://localhost:3003/api'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
 
-export function getList() {
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
+export function getList () {
   const request = axios.get(`${BASE_URL}/billingCycles`)
   return {
     type: 'BILLING_CYCLES_FETCHED',
     payload: request
+  }
+}
+
+export function create (values) {
+  return dispatch => {
+    axios.post(`${BASE_URL}/billingCycles`, values)
+      .then(resp => {
+        toastr.success('Sucesso', 'Operação realizada com sucesso.')
+        dispatch([
+          resetForm('billingCycleForm'),
+          getList(),
+          selectTab('tabList'),
+          showTabs('tabList', 'tabCreate')
+        ])
+      }).catch(e => {
+        e.response.data.errors.forEach(error => toastr.error('Erro', error))
+      })
+    return {
+      type: 'TEMP'
+    }
   }
 }
