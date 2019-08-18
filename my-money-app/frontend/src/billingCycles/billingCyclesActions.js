433dadadac161e35da98dadf435b65d5ccd9efc8
EzequielGnich <ezequielgnich@live.com>
2019-08-18 19:50:09 -0300

    Refatorado a função create() e adicionado três novas funções update(), remove() e submit(), função submit é genérica e serve  para todas aas funções como create, update e remove

--- my-money-app/frontend/src/billingCycles/billingCyclesActions.js
+++ my-money-app/frontend/src/billingCycles/billingCyclesActions.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:3003/api'
 import { toastr } from 'react-redux-toastr'
-import { reset as resetForm } from 'redux-form'
+import { reset as resetForm, initialize } from 'redux-form'
 
 import { showTabs, selectTab } from '../common/tab/tabActions'
+
+const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
 
 export function getList () {
   const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -14,16 +16,27 @@ export function getList () {
 }
 
 export function create (values) {
+  return submit(values, 'post')
+}
+
+export function update(values) {
+  return submit(values, 'put')
+}
+
+export function remove(values) {
+  return submit(values, 'delete')
+}
+
+function submit(values, method) {
   return dispatch => {
-    axios.post(`${BASE_URL}/billingCycles`, values)
+    // Variavel responsavel por armazenar o id do usuario id vem do values._id
+    // criado no mongodb feita a verificação se existe um valor para id senão
+    // adiciona uma string vazia deixando a URL da mesma forma
+    const id = values._id ? values._id : ''
+    axios[method](`${BASE_URL}/billingCycles/${id}`, values)
       .then(resp => {
         toastr.success('Sucesso', 'Operação realizada com sucesso.')
-        dispatch([
-          resetForm('billingCycleForm'),
-          getList(),
-          selectTab('tabList'),
-          showTabs('tabList', 'tabCreate')
-        ])
+        dispatch(init())
       }).catch(e => {
         e.response.data.errors.forEach(error => toastr.error('Erro', error))
       })
@@ -31,4 +44,38 @@ export function create (values) {
       type: 'TEMP'
     }
   }
+}
+
+export function showUpdate(billingCycle) {
+  return [
+    showTabs('tabUpdate'),
+    selectTab('tabUpdate'),
+    initialize('billingCycleForm', billingCycle)
+  ]
+}
+
+export function showDelete(billingCycle) {
+  return [
+    showTabs('tabDelete'),
+    selectTab('tabDelete'),
+    initialize('billingCycleForm', billingCycle)
+  ]
+}
+
+/* Reutilizar este código
+export function showUpdate(billingCycle) {
+  return [
+    showTabs('tabUpdate'),
+    selectTab('tabUpdate'),
+    initialize('billingCycleForm', billingCycle)
+  ]
+}
+*/
+export function init() {
+  return [
+    showTabs('tabList', 'tabCreate'),
+    selectTab('tabList'),
+    getList(),
+    initialize('billingCycleForm', INITIAL_VALUES)
+  ]
 }
