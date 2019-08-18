42af472b41a87c767e87f8da9fac5b9c4dd31d62
EzequielGnich <ezequielgnich@live.com>
2019-08-18 17:54:44 -0300

    Aplicado as mensagens do toastr

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -5,14 +5,17 @@ import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messages from '../common/msgs/msg'
 
 export default props => (
   <div className='wrapper'>
     <Header />
-    <Sidebar />
+    <Sidebar />npm run dev
+
     <div className='content-wrapper'>
       <Routes />
     </div>
     <Footer />
+    <Messages />
   </div>
 )
