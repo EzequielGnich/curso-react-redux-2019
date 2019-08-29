044890f8a858bcc5dde37adcf2318cdd3f3f6992
EzequielGnich <ezequielgnich@live.com>
2019-08-29 19:51:31 -0300

    Refatoração do códgo com prettier para padronização dos arquivos

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
+import React from "react";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
+import { IndexRoute } from "react-router";
 
-import Dashboard from '../dashboard/dashboard'
-import BillingCycle from '../billingCycles/billingCycles'
+import Dashboard from "../dashboard/dashboard";
+import BillingCycle from "../billingCycles/billingCycles";
 
 export default props => (
-  <Router  >
-    <Route path='/' exact component={BillingCycle} />
-    <Route path='/billingCycles' exact component={Dashboard} />
-    <Redirect from='*' to='/' />
+  <Router>
+    <Route path="/" exact component={BillingCycle} />
+    <Route path="/billingCycles" exact component={Dashboard} />
+    <Redirect from="*" to="/" />
   </Router>
-)
+);
