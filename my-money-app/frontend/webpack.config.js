7b53523b5653a7eda7af7716e1fdc8c2b7e73eb7
EzequielGnich <ezequielgnich@live.com>
2019-08-29 20:18:32 -0300

    Refatorado o código para manter o padrão dos arquivos

--- my-money-app/frontend/webpack.config.js
+++ my-money-app/frontend/webpack.config.js
@@ -1,48 +1,52 @@
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const path = require('path')
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: './app.js'
+    path: path.resolve(__dirname, "public"),
+    filename: "./app.js"
   },
   devServer: {
     port: 8080,
-    contentBase: './public'
+    contentBase: "./public"
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ["", ".js", ".jsx"],
     alias: {
-      modules: path.resolve(__dirname, 'node_modules/'),
-      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-      bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.min.js'
+      modules: path.resolve(__dirname, "node_modules/"),
+      jquery: "modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js",
+      bootstrap: "modules/admin-lte/bootstrap/js/bootstrap.min.js"
     }
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
     }),
-    new ExtractTextPlugin('app.css')
+    new ExtractTextPlugin("app.css")
   ],
   module: {
-    loaders: [{
-      test: /.js[x]?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react'],
-        plugins: ['transform-object-rest-spread']
+    loaders: [
+      {
+        test: /.js[x]?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015", "react"],
+          plugins: ["transform-object-rest-spread"]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+        loader: "file-loader"
       }
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    }, {
-      test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-      loader: 'file-loader'
-    }]
+    ]
   }
-}
+};
