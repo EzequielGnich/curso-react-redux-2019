fc67a6a305e7d3022abc5528ed8e4778f1ae0fc0
EzequielGnich <ezequielgnich@live.com>
2019-08-29 19:31:02 -0300

    Criado uma tag span devido a um erro no css

--- my-money-app/frontend/src/common/template/menuItem.jsx
+++ my-money-app/frontend/src/common/template/menuItem.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default props => (
   <li>
     <a href={props.path}>
       <i className={`fa fa-${props.icon}`} />
-      {props.label}
+      <span>{props.label}</span>
     </a>
   </li>
-)
+);
