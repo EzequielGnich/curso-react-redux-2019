693a6f7fff1b260a11f36f69f3b435838c93d4ab
EzequielGnich <ezequielgnich@live.com>
2019-08-29 19:49:06 -0300

    Refatoração do código com prettier para haver uma padronização no código

--- my-money-app/frontend/src/billingCycles/billingCycles.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycles.jsx
@@ -1,68 +1,73 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-import ContentHeader from '../common/template/contentHeader'
-import Content from '../common/template/content'
-import Tabs from '../common/tab/tabs'
-import TabsHeader from '../common/tab/tabsHeader'
-import TabsContent from '../common/tab/tabsContent'
-import TabHeader from '../common/tab/tabHeader'
-import TabContent from '../common/tab/tabContent'
-import BillingCycleList from './billingCyclesList'
-import Form from './billingCycleForm'
-import { init, create, update, remove } from './billingCyclesActions'
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
+import TabContent from "../common/tab/tabContent";
+import BillingCycleList from "./billingCyclesList";
+import Form from "./billingCycleForm";
+import { init, create, update, remove } from "./billingCyclesActions";
 
 class BillingCycle extends Component {
-
   componentWillMount() {
-    this.props.init()
+    this.props.init();
   }
 
   render() {
     return (
       <div>
-        <ContentHeader title='Ciclo de pagamentos' small='Cadastro' />
+        <ContentHeader title="Ciclo de pagamentos" small="Cadastro" />
         <Content>
           <Tabs>
             <TabsHeader>
-              <TabHeader label='Listar' icon='bars' target='tabList' />
-              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
-              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
-              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
             </TabsHeader>
             <TabsContent>
-              <TabContent id='tabList'>
+              <TabContent id="tabList">
                 <BillingCycleList />
               </TabContent>
-              <TabContent id='tabCreate'>
+              <TabContent id="tabCreate">
                 <Form
                   onSubmit={this.props.create}
-                  submitLabel='Incluir'
-                  submitClass='primary' />
+                  submitLabel="Incluir"
+                  submitClass="primary"
+                />
               </TabContent>
-              <TabContent id='tabUpdate'>
+              <TabContent id="tabUpdate">
                 <Form
                   onSubmit={this.props.update}
-                  submitLabel='Alterar'
-                  submitClass='info' />
+                  submitLabel="Alterar"
+                  submitClass="info"
+                />
               </TabContent>
-              <TabContent id='tabDelete'>
+              <TabContent id="tabDelete">
                 <Form
                   onSubmit={this.props.remove}
                   readOnly={true}
-                  submitLabel='Excluir'
-                  submitClass='danger' />
+                  submitLabel="Excluir"
+                  submitClass="danger"
+                />
               </TabContent>
             </TabsContent>
           </Tabs>
         </Content>
       </div>
-    )
+    );
   }
 }
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({init, create, update, remove}, dispatch);
+  bindActionCreators({ init, create, update, remove }, dispatch);
 
-export default connect(null, mapDispatchToProps)(BillingCycle)
+export default connect(
+  null,
+  mapDispatchToProps
+)(BillingCycle);
