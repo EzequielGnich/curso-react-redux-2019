cdd1be6175eecda58dcf464c5deaa160a4ff2ba1
EzequielGnich <ezequielgnich@live.com>
2019-08-29 19:50:31 -0300

    Refatoração do códgo com prettier para padronização dos arquivos

--- my-money-app/frontend/src/billingCycles/billingCyclesActions.js
+++ my-money-app/frontend/src/billingCycles/billingCyclesActions.js
@@ -1,30 +1,30 @@
-import axios from 'axios'
-import { toastr } from 'react-redux-toastr'
-import { reset as resetForm, initialize } from 'redux-form'
+import axios from "axios";
+import { toastr } from "react-redux-toastr";
+import { reset as resetForm, initialize } from "redux-form";
 
-import { showTabs, selectTab } from '../common/tab/tabActions'
+import { showTabs, selectTab } from "../common/tab/tabActions";
 
-const BASE_URL = 'http://localhost:3003/api'
-const INITIAL_VALUES = {credits: [{}], debts: [{}]}
+const BASE_URL = "http://localhost:3003/api";
+const INITIAL_VALUES = { credits: [{}], debts: [{}] };
 
-export function getList () {
-  const request = axios.get(`${BASE_URL}/billingCycles`)
+export function getList() {
+  const request = axios.get(`${BASE_URL}/billingCycles`);
   return {
-    type: 'BILLING_CYCLES_FETCHED',
+    type: "BILLING_CYCLES_FETCHED",
     payload: request
-  }
+  };
 }
 
-export function create (values) {
-  return submit(values, 'post')
+export function create(values) {
+  return submit(values, "post");
 }
 
 export function update(values) {
-  return submit(values, 'put')
+  return submit(values, "put");
 }
 
 export function remove(values) {
-  return submit(values, 'delete')
+  return submit(values, "delete");
 }
 
 function submit(values, method) {
@@ -32,34 +32,35 @@ function submit(values, method) {
     // Variavel responsavel por armazenar o id do usuario id vem do values._id
     // criado no mongodb feita a verificação se existe um valor para id senão
     // adiciona uma string vazia deixando a URL da mesma forma
-    const id = values._id ? values._id : ''
+    const id = values._id ? values._id : "";
     axios[method](`${BASE_URL}/billingCycles/${id}`, values)
       .then(resp => {
-        toastr.success('Sucesso', 'Operação realizada com sucesso.')
-        dispatch(init())
-      }).catch(e => {
-        e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        toastr.success("Sucesso", "Operação realizada com sucesso.");
+        dispatch(init());
       })
+      .catch(e => {
+        e.response.data.errors.forEach(error => toastr.error("Erro", error));
+      });
     return {
-      type: 'TEMP'
-    }
-  }
+      type: "TEMP"
+    };
+  };
 }
 
 export function showUpdate(billingCycle) {
   return [
-    showTabs('tabUpdate'),
-    selectTab('tabUpdate'),
-    initialize('billingCycleForm', billingCycle)
-  ]
+    showTabs("tabUpdate"),
+    selectTab("tabUpdate"),
+    initialize("billingCycleForm", billingCycle)
+  ];
 }
 
 export function showDelete(billingCycle) {
   return [
-    showTabs('tabDelete'),
-    selectTab('tabDelete'),
-    initialize('billingCycleForm', billingCycle)
-  ]
+    showTabs("tabDelete"),
+    selectTab("tabDelete"),
+    initialize("billingCycleForm", billingCycle)
+  ];
 }
 
 /* Reutilizar este código
@@ -73,9 +74,9 @@ export function showUpdate(billingCycle) {
 */
 export function init() {
   return [
-    showTabs('tabList', 'tabCreate'),
-    selectTab('tabList'),
+    showTabs("tabList", "tabCreate"),
+    selectTab("tabList"),
     getList(),
-    initialize('billingCycleForm', INITIAL_VALUES)
-  ]
+    initialize("billingCycleForm", INITIAL_VALUES)
+  ];
 }
