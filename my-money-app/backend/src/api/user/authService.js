7d5c800063ae7fd9b3a690363b885988dcff3c21
EzequielGnich <ezequielgnich@live.com>
2019-08-30 14:51:30 -0300

    Criado arquivo authService.js responsavel por fazer a autenticação dos usuarios

---
+++ my-money-app/backend/src/api/user/authService.js
@@ -0,0 +1,48 @@
+const _ = require("lodash");
+const JWT = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./user");
+
+// Regex para validar um email
+const emailRegex = /\S+@\S+\.\S+/;
+
+// Regex para validar uma senha
+// A senha deverá ter digitos (?=.*\d)
+// A senha deverá ter letras minusculas de a até z (?=.*[a-z])
+// A senha deverá ter letras maisculas de A até Z (?=*[A-Z])
+// A senha deverá ter caracteres especiais (?=.*[@#$%])
+// A senha deverá ter entre 6 e 20 caracteres {6.20}
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=*[A-Z])(?=.*[@#$%]).{6.20})/;
+
+const sendErrorsFromDB = (res, dbErrors) => {
+  const errors = [];
+  _.forIn(dbErrors.errors, error => errors.push(error.message));
+  return res.status(400).json({ errors });
+};
+
+const login = (req, res, next) => {
+  const email = req.body.email || "";
+  const password = req.body.password || "";
+
+  User.findOne({ email }, (res, user) => {
+    if (err) {
+      return sendErrorsFromDB(res, err);
+    } else if (user && bcrypt.compareSync(password, user.password)) {
+      const token = JWT.sign(user, process.env.AUTH_SECRET, {
+        expiresIn: "1 day"
+      });
+      const { name, email } = user;
+      res.json({ name, email, token });
+    } else {
+      return res.status(400).send({ errors: ["Usuário/Senha inválidos"] });
+    }
+  });
+};
+
+const validateToken = (req, res, next) => {
+  const token = req.body.token || "";
+
+  JWT.verify(token, process.env.AUTH_SECRET, function(err, decoded) {
+    return res.status(200).send({ valid: !err });
+  });
+};
