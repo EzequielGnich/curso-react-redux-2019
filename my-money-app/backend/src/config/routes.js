3021aa03b5117dcbf7d51367006f8724426e34b5
EzequielGnich <ezequielgnich@live.com>
2019-08-30 16:30:35 -0300

    Definida as rotas protegidas e as abertas da aplicação que não precisam de uma validação de token

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require("express");
+const auth = require("./auth");
 
-module.exports = function (server) {
-  // Define URL base
-  const router = express.Router()
-  server.use('/api', router)
+module.exports = function(server) {
+  /*
+   * ROTAS PROTEGIDAS POR TOKEN
+   */
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
 
-  // Rotas do ciclo de pagamento
-  const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
-}
+  protectedApi.use(auth);
+
+  const BillingCycle = require("../api/billingCycle/billingCycleService");
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  /*
+   * ROTAS ABERTAS
+   */
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+
+  const AuthService = require("../api/user/authService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("validateToken", AuthService.validateToken);
+};
