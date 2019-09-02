6317005e6172d5659e7ef36d067f772ef27a5cbe
EzequielGnich <ezequielgnich@live.com>
2019-09-02 16:46:06 -0300

    Alterado o arquivo para manter a formatação padrão do prjeto

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
-import { Provider } from 'react-redux'
-import promise from 'redux-promise'
-import multi from 'redux-multi'
-import thunk from 'redux-thunk'
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import promise from "redux-promise";
+import multi from "redux-multi";
+import thunk from "redux-thunk";
 
-import App from './main/app'
-import Reducers from './main/reducers'
+import App from "./main/app";
+import Reducers from "./main/reducers";
 
 // configuração do devTools
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-  && window.__REDUX_DEVTOOLS_EXTENSION__()
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = applyMiddleware(multi, thunk, promise)(createStore)(Reducers, devTools)
-const rootElement = document.getElementById('app')
+const store = applyMiddleware(multi, thunk, promise)(createStore)(
+  Reducers,
+  devTools
+);
+const rootElement = document.getElementById("app");
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-  , rootElement)
+  </Provider>,
+  rootElement
+);
