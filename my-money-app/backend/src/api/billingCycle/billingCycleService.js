a8352684ce58ea164a8f521a47959ed5241970cf
EzequielGnich <ezequielgnich@live.com>
2019-09-02 19:08:22 -0300

    Código refatorado para manter o padrão dos arquivos

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,40 +1,50 @@
-const Sentry = require('@sentry/node')
-const BillingCycle = require('./billingCycle')
-const errorHandler = require('../common/errorHandler')
+const Sentry = require("@sentry/node");
+const BillingCycle = require("./billingCycle");
+const errorHandler = require("../common/errorHandler");
 
-Sentry.init({ dsn: 'https://605340cce1f84c93996961a81f73d97a@sentry.io/1513614' })
+Sentry.init({
+  dsn: "https://605340cce1f84c93996961a81f73d97a@sentry.io/1513614"
+});
 
-BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({ new: true, runValidators: true })
-BillingCycle.after('post', errorHandler).after('put', errorHandler)
+BillingCycle.methods(["get", "post", "put", "delete"]);
+BillingCycle.updateOptions({ new: true, runValidators: true });
+BillingCycle.after("post", errorHandler).after("put", errorHandler);
 
-BillingCycle.route('count', (req, res, next) => {
+BillingCycle.route("count", (req, res, next) => {
   BillingCycle.countDocuments((error, value) => {
     if (error) {
-      res.status(500).json({ errors: [error] })
+      res.status(500).json({ errors: [error] });
     } else {
-      res.json({ value })
+      res.json({ value });
     }
-  })
-})
+  });
+});
 
-BillingCycle.route('sumary', (req, res, next) => {
-  BillingCycle.aggregate([{
-    $project: { credit: { $sum: '$credits.value' },
-      debt: { $sum: '$debts.value' } }
-  }, {
-    $group: { _id: null,
-      credit: { $sum: '$credit' },
-      debt: { $sum: '$debt' } }
-  }, {
-    $project: { _id: 0, credit: 1, debt: 1 }
-  }], (error, result) => {
+BillingCycle.route("sumary", (req, res, next) => {
+  BillingCycle.aggregate([
+    {
+      $project: {
+        credit: { $sum: "$credits.value" },
+        debt: { $sum: "$debts.value" }
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        credit: { $sum: "$credit" },
+        debt: { $sum: "$debt" }
+      }
+    },
+    {
+      $project: { _id: 0, credit: 1, debt: 1 }
+    }
+  ]).exec((error, result) => {
     if (error) {
-      res.status(500).json({ errors: [error] })
+      res.status(500).json({ errors: [error] });
     } else {
-      res.json(result[0] || { credit: 0, debt: 0 })
+      res.json(result[0] || { credit: 0, debt: 0 });
     }
-  })
-})
+  });
+});
 
-module.exports = BillingCycle
+module.exports = BillingCycle;
