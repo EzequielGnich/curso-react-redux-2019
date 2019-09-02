c5b9c269e13340c1eaf0595dcbcbc04da705666d
EzequielGnich <ezequielgnich@live.com>
2019-09-02 16:41:56 -0300

    Alterado a estrutura das rotas

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { HashRouter as Router, Route, Redirect } from "react-router-dom";
-import { IndexRoute } from "react-router";
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  IndexRoute
+} from "react-router";
 
+import AuthOrApp from "./authOrApp";
 import Dashboard from "../dashboard/dashboard";
 import BillingCycle from "../billingCycles/billingCycles";
 
 export default props => (
   <Router>
-    <Route path="/" exact component={BillingCycle} />
-    <Route path="/billingCycles" exact component={Dashboard} />
+    <Route path="/" component={AuthOrApp}>
+      <IndexRoute component={Dashboard} />
+      <Route path="billingCycles" component={BillingCycle} />
+    </Route>
     <Redirect from="*" to="/" />
   </Router>
 );
