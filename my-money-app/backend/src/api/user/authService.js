1af1150eb699222dcaf945eb968ecbda1e81416e
EzequielGnich <ezequielgnich@live.com>
2019-09-02 19:10:29 -0300

    Alterado a forma como o user é passado para o frontend

--- my-money-app/backend/src/api/user/authService.js
+++ my-money-app/backend/src/api/user/authService.js
@@ -28,7 +28,7 @@ const login = (req, res, next) => {
     if (err) {
       return sendErrorsFromDB(res, err);
     } else if (user && bcrypt.compareSync(password, user.password)) {
-      const token = JWT.sign(user, process.env.AUTH_SECRET, {
+      const token = JWT.sign({ ...user }, process.env.AUTH_SECRET, {
         expiresIn: "1 day"
       });
       const { name, email } = user;
