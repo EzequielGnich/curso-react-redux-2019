ce76c6016aff212a0013f79cf86e64411c9bc811
EzequielGnich <ezequielgnich@live.com>
2019-09-02 16:42:41 -0300

    Alterado a formatação do arquivo para seguir o padrão de projeto

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,17 +1,19 @@
-import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
-import { reducer as toastrReducer } from 'react-redux-toastr'
+import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+import { reducer as toastrReducer } from "react-redux-toastr";
 
-import DashboardReducer from '../dashboard/dashboardReducer'
-import TabReducer from '../common/tab/tabReducer'
-import BillingCyclesReducer from '../billingCycles/billingCyclesReducer'
+import DashboardReducer from "../dashboard/dashboardReducer";
+import TabReducer from "../common/tab/tabReducer";
+import BillingCyclesReducer from "../billingCycles/billingCyclesReducer";
+import AuthReducer from "../auth/authReducer";
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
   tab: TabReducer,
   billingCycle: BillingCyclesReducer,
   form: formReducer,
-  toastr: toastrReducer
-})
+  toastr: toastrReducer,
+  auth: AuthReducer
+});
 
-export default rootReducer
+export default rootReducer;
