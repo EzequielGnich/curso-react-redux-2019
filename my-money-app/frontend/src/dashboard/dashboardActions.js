57eebd81abb9824a4dd715f5ca18d711c38dbaf8
EzequielGnich <ezequielgnich@live.com>
2019-09-03 13:09:21 -0300

    Erro de digitação na url

--- my-money-app/frontend/src/dashboard/dashboardActions.js
+++ my-money-app/frontend/src/dashboard/dashboardActions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getSummary () {
-  const request = axios.get(`${BASE_URL}/billingCycles/sumary`)
+  const request = axios.get(`${BASE_URL}/billingCycles/summary`)
   return {
     type: 'BILLING_SUMMARY_FETCHED',
     payload: request
