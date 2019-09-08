0967ac1be751cfdcaf1ec0fad7a49ac1c68e1b9f
EzequielGnich <ezequielgnich@live.com>
2019-09-08 12:23:17 -0300

    Criado variavel para mostrar o elemento principal da aplicação

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
+const rootElement = <h1>React</h1>
+ReactDOM.render(rootElement, document.getElementById('root'))
