47694a2c7b54e57031b3509d13b1ec4038be6937
EzequielGnich <ezequielgnich@live.com>
2019-09-08 14:16:23 -0300

    Trabalhando com multiplos elementos

---
+++ react/exercicios/src/components/multiplesElements.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const BoaTarde = (props) => <h1>Boa tarde {props.name}</h1>
+
+export const BoaNoite = (props) => <h1>Boa noite {props.name}</h1>
+
+export default {
+  BoaNoite,
+  BoaTarde
+}
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Primeiro compoenente simples
 // import First from './components/first'
-import ComponentWithProps from './components/componentWithProps'
 
-const rootElement = <ComponentWithProps name="Ezequiel" />
+// Componente que recebe propriedades
+// import ComponentWithProps from './components/componentWithProps'
+
+
+/* importando componentes que foram exportados com export const _________
+ * import { BoaNoite, BoaTarde } from './components/multiplesElements'
+ * const rootElement = [
+ * <BoaNoite key="BoaNoite" name="Ezequiel" />,
+ * <BoaTarde key="BoaTarde" name="Ezequiel" />
+ * ]
+ */
+
+/* Importando elementos que foram exportados com export default {}
+ *import Multi from './components/multiplesElements'
+ *
+ *const rootElement = [
+ *  <Multi.BoaNoite key="BoaNoite" name="Ezequiel" />,
+ *  <Multi.BoaTarde key="BoaTarde" name="Ezequiel" />
+ *]
+ */
+
 ReactDOM.render(rootElement, document.getElementById('root'))
