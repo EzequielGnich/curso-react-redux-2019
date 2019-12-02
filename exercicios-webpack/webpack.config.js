874c02b0e7fc23f71129981f5258df3a37ee227c
EzequielGnich <ezequielgnich@live.com>
2019-12-02 19:32:24 -0300

    adicinado primeiras configurações do babel

--- exercicios-webpack/webpack.config.js
+++ exercicios-webpack/webpack.config.js
@@ -10,5 +10,17 @@ module.exports = {
   devServer: {
     port: 3333,
     contentBase: "./public"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/, // Faz a leitura nos arquivos da aplicação que terminam com .js
+        loader: "babel-loader", //Chama o babel-loader para fazer a tradução do código javascript para um código que o browser entenda
+        exclude: /node_modules/, // Exclui a pasta node_modules da leitura dos arquivos
+        query: {
+          presets: ["es2015"] // Utiliza o dicionario do es2015 para traduzir o código para o browser
+        }
+      }
+    ]
   }
 };
