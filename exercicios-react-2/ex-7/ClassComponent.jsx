221419eb65b109f6a4ae003ae6d485a80fa918c0
EzequielGnich <ezequielgnich@live.com>
2019-12-03 21:51:49 -0300

    exercicio 7

---
+++ exercicios-react-2/ex-7/ClassComponent.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
---
+++ exercicios-react-2/ex-7/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ClassComponent from "./ClassComponent";
+
+ReactDOM.render(
+  <ClassComponent value="Componente em forma de classe" />,
+  document.getElementById("app")
+);
