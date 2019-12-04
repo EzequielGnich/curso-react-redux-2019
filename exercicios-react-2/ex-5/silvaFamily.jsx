55ddbcb3af26d635717f2ad8b1dda786cfa131e8
EzequielGnich <ezequielgnich@live.com>
2019-12-03 21:53:36 -0300

    exercicio 5

---
+++ exercicios-react-2/ex-5/silvaFamily.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Member from "./member";
+
+const SilvaFamily = props => (
+  <div>
+    <Member name="Joao" lastname="Silva" />
+    <Member name="Carlos" lastname="Silva" />
+    <Member name="Antonio" lastname="Silva" />
+  </div>
+);
+
+export { SilvaFamily };
