3c4bb7db9b55d090dfecd9477555ba1f93d2400b
EzequielGnich <ezequielgnich@live.com>
2019-12-03 22:31:58 -0300

    exercicios 8

---
+++ exercicios-react-2/ex-8/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ClassComponent from "./ClassComponent";
+
+ReactDOM.render(
+  <ClassComponent label="Contador" initialValue={10} />,
+  document.getElementById("app")
+);
