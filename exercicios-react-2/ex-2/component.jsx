79073cb57739864b8805c0d1f71bb976384c19a1
EzequielGnich <ezequielgnich@live.com>
2019-12-03 21:53:49 -0300

    exercicio 2

---
+++ exercicios-react-2/ex-2/component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+  return <h1>Componente Arrow Function</h1>;
+};
+
+// Utilizando parenteses para retornar implicitamente o componente
+// export default () => (
+//   <h1>Componente Arrow Function</h1>
+// );
+
+// Componente em uma linha apenas
+// export default () => <h1>Componente Arrow Function</h1>;
